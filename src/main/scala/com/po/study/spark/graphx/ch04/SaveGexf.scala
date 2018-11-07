00fbd9e65b5d51833ad2c1de96f63923f3f31b06
ZJ <zhangjie@megvii.com>
2018-11-07 10:53:12 +0800

    update save gexf

--- src/main/scala/com/po/study/spark/graphx/ch04/SaveGexf.scala
+++ src/main/scala/com/po/study/spark/graphx/ch04/SaveGexf.scala
@@ -3,6 +3,7 @@ package com.po.study.spark.graphx.ch04
 import java.io.PrintWriter
 
 import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx.util.GraphGenerators
 
 /**
   * Created by ZJ on 05/11/2018.
@@ -17,4 +18,23 @@ object SaveGexf extends App {
   val pw = new PrintWriter("src/main/resources/tmp/gexf/demo1.gexf")
   pw.write(toGexf(InitGraph.graph))
   pw.close()
+
+  // generate grid graph
+  val pw1 = new PrintWriter("src/main/resources/tmp/gexf/demo2.gexf")
+  pw1.write(toGexf(GraphGenerators.gridGraph(InitGraph.sc, 4, 4)))
+  pw1.close()
+
+  // generate star graph
+  val pw2 = new PrintWriter("src/main/resources/tmp/gexf/demo3.gexf")
+  pw2.write(toGexf(GraphGenerators.starGraph(InitGraph.sc, 8)))
+  pw2.close()
+
+  // generate log normal graph
+  val pw3 = new PrintWriter("src/main/resources/tmp/gexf/demo4.gexf")
+  val graph3 = GraphGenerators.logNormalGraph(InitGraph.sc, 20)
+  pw3.write(toGexf(graph3))
+  println(graph3.aggregateMessages[Int](_.sendToSrc(1), _ + _).map(_._2).collect().sorted)
+  pw3.close()
+
+  //
 }
